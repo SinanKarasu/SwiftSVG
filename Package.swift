fefbd081b325a9703b999cce563aaa8c1ebac539
Sinan Karasu <sinan@bozuk.org>
2025-08-13 18:47:29 -0700

    Minimum OS Platform additiom, and Swift Tools to 5.9

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftSVG",
+	platforms: [
+		.macOS(.v13),
+			// optionally add .iOS(.v15), .tvOS(.v15), .watchOS(.v8) if needed
+			],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
